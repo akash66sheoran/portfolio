694abc9fc7a7cfd2d11d3f03a224372e0ff682aa
Akash <56023730+akash66sheoran@users.noreply.github.com>
2023-10-09 08:24:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,6 +19,12 @@ resource "aws_key_pair" "portfolio_key" {
 resource "aws_security_group" "my_security_group" {
   name = "my-security-group"
   ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     from_port = 5000
     to_port = 5000
     protocol = "tcp"
