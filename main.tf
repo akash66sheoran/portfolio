3982bad591bfb2cf5c5c83603bdf14fd44cd03dc
Akash <56023730+akash66sheoran@users.noreply.github.com>
2023-10-09 08:14:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,10 +16,27 @@ resource "aws_key_pair" "my_key_pair" {
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
+resource "aws_security_group" "my_security_group" {
+  name = "my-security-group"
+  ingress {
+    from_port = 5000
+    to_port = 5000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "portfolio_website" {
   ami = "ami-0f5ee92e2d63afc18"
   instance_type = "t2.micro"
   key_name = aws_key_pair.my_key_pair.key_name
+  security_groups = ["my-security-group"]
   tags = {
     Name = "portfolio"
   }
